197a39258d710cef00cb561625929ce3da313d91
Farah alsoqi <farahmanna97@gmail.com>
2023-10-12 19:33:13 +0300

    Done I Think

--- index.js
+++ index.js
@@ -4,5 +4,17 @@ const example = [
   ["A", "B", "A"]];
 
 const whereIsWaldo = (matrix) => {
-  
+let different=null;
+for (let i=0; i<matrix.length; i++){
+for (let f=0; f<matrix.length; f++ ){
+  if (i === 0 && f === 0) {
+    different = matrix[i][f];}
+ else if (matrix[i][f] !== matrix[0][0]) {
+    different = matrix[i][f];}
 }
+}
+return different;
+
+}
+const result = whereIsWaldo(example);
+console.log(result);
